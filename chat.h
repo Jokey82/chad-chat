d3d2fbed6d84a5a9424433a4ebdf3ea427876353
Andrew <41803632+Jokey82@users.noreply.github.com>
2019-10-28 21:31:04 +0200

    Fix

--- chat.h
+++ chat.h
@@ -6,13 +6,26 @@
 #include <arpa/inet.h>
 #include<unistd.h>
 #include<string.h>
+#include<pthread.h>
 #include<ncurses.h>
 
+//common
 #define ALT_BACKSPACE 127
-#define MAX 128
+#define MAX 128 //common buffer size
+#define MAX_UNAME 36
 #define BYE_MESSAGE 10101010
 
-void send_mode(int socket);
-void recv_mode(int socket);	
+//server-specific
+#define MAX_THREADS 4
+#define MAX_CLIENTS 4
+
+//client-specific
 void chat_loop(int socket);
+int send_uname(int socket);
+int send_pass(int socket);
+//server-specific
+
+//common
 int check_buff(char* buffer, size_t n);
+int find_free(int array[], size_t size);
+int find_string_in_array(char** array, char* entry,  size_t arr_len);
